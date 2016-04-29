42fed9b78fd3bf33ab4e0b7525ad0d0b8494e7b5
Chris-Kay <chris.kay01@bbc.co.uk>
2016-04-29 09:40:46 +0100

    Taking paramters

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 var libxmljs = require('libxmljs-mt');
+var fs = require('file-system');
+
+
+convertXml(process.argv.slice(2)[0]);
 
 function convertXml(xml) {
-	var parsedXml = libxmljs.parseXml(xml);
-
+    var passsedXml = fs.readFileSync(xml, "utf-8");
+	var parsedXml = libxmljs.parseXml(passsedXml);
 	var gchild = parsedXml.get('//CD//TITLE').text();
 
-
+	console.log(gchild);
 	return gchild;
 }
 
